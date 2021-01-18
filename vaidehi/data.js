cf169ec0a251fc8be2cee1b2561fd85174573c83
vaidehidarji1224 <65633847+vaidehidarji1224@users.noreply.github.com>
2021-01-18 13:38:35 -0500

    Add files via upload

---
+++ vaidehi/data.js
@@ -0,0 +1,23 @@
+/* here are the arrays used for project 1 */
+
+var filenames = ["106020.jpg", "116010.jpg", "120010.jpg"];
+var titles = ["Girl with a Pearl Earring", "Artist Holding a Thistle", "Portrait of Eleanor of Toledo"];
+var quantities = [5, 1, 2];
+var prices = [80, 125, 75];
+
+/* 
+   NOTE: parallel arrays are not an ideal way to represent this data.
+         We have done this to simplify this exercise.
+         
+         A better approach would be to turn these parallel arrays
+         into an array of cart item objects. Objects are used in
+         projects 2 and 3.
+*/
+
+var dataArray = [];
+
+dataArray[0] = { 'price': prices[0], 'quantities':quantities[0] };
+dataArray[1] = { 'price': prices[1], 'quantities':quantities[1] };
+dataArray[2] = { 'price': prices[2], 'quantities': quantities[2] };
+
+//console.log(dataArray);
