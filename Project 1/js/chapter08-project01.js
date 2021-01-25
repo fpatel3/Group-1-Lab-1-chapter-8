b40fefd128e4f8d67018dee26ebc9e3b01a69f5d
fpatel3 <forampatel3010@gmail.com>
2021-01-25 09:11:49 -0500

    made changes

--- Project 1/js/chapter08-project01.js
+++ Project 1/js/chapter08-project01.js
@@ -1,19 +1,18 @@
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
 
-   function loadcart() {
-      var subtotal = 0.0, tax, shipping, grandtotal;
-  
-      shipping = subtotal > 1000 ? 0 : 40;
-      grandtotal = subtotal + tax + shipping;
-  
-  
-      outputTotalsRow("subtotal", subtotal, "Amount");
-      outputTotalsRow("tax", tax, "Amount");
-      outputTotalsRow("shipping", shipping, "Amount");
-      outputTotalsRow("grandtotal", grandtotal, "totalAmount");
-  
-  }
-  
-  
-  
+function loadcart() {
+    var subtotal = 0.0, tax, shipping, grandtotal;
+
+    shipping = subtotal > 1000 ? 0 : 40;
+    grandtotal = subtotal + tax + shipping;
+
+
+    outputTotalsRow("subtotal", subtotal, "Amount");
+    outputTotalsRow("tax", tax, "Amount");
+    outputTotalsRow("shipping", shipping, "Amount");
+    outputTotalsRow("grandtotal", grandtotal, "totalAmount");
+
+}
+
+
